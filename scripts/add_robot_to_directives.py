2f8beb377b90f111f56d9431881cf77639e2f644
cohnt <cohnt@umich.edu>
2026-01-08 14:56:47 -0500

    Use a photorealistic model of the WSG schunk, and set the default finger positions to be open (matching the model with welded fingers).

--- scripts/add_robot_to_directives.py
+++ scripts/add_robot_to_directives.py
@@ -28,7 +28,10 @@ def make_robot_and_gripper_yaml(start_xy):
 
 - add_model:
     name: wsg_50
-    file: package://drake_models/wsg_50_description/sdf/schunk_wsg_50.sdf
+    file: package://drake_models/wsg_50_description/sdf/schunk_wsg_50_no_tip.sdf
+    default_joint_positions:
+      left_finger_sliding_joint: [-0.054]
+      right_finger_sliding_joint: [0.054]
 - add_weld:
     parent: mobile_iiwa::iiwa_link_7
     child: wsg_50::body
